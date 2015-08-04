be2208b99fc77e51003bf2cd70c490262f9a4415
Michael Rodrigues <mikebrodrigues@gmail.com>
2015-08-03 18:38:45 -0700

    refactored and DRYed up BookMethods

--- lib/orderbook/book_methods.rb
+++ lib/orderbook/book_methods.rb
@@ -1,7 +1,7 @@
-require 'bigdecimal' 
+require 'bigdecimal'
 class Orderbook
   # This class provides methods to apply updates to the state of the orderbook
-  # as they come in as individual messages.
+  # as they are received by the websocket.
   #
   module BookMethods
     BIGDECIMAL_KEYS = ['size', 'old_size', 'new_size', 'remaining_size', 'price', 'funds', 'old_funds', 'new_funds']
@@ -24,71 +24,51 @@ class Orderbook
     private
 
     def open(msg)
-      order = { price: msg.fetch('price'), size: msg.fetch('remaining_size'), order_id: msg.fetch('order_id') }
-      case msg.fetch('side')
-      when 'buy'
-        @bids << order
-      when 'sell'
-        @asks << order
-      end
+      order = {
+        price:    msg.fetch('price'),
+        size:     msg.fetch('remaining_size'),
+        order_id: msg.fetch('order_id')
+      }
+
+      @bids << order if msg.fetch('side') == 'buy'
+      @asks << order if msg.fetch('side') == 'sell'
     end
 
     def match(msg)
-      case msg.fetch('side')
-      when 'sell'
-        @asks.map do |ask|
-          if ask.fetch(:order_id) == msg.fetch('maker_order_id')
-            ask[:size] = ask[:size] - msg.fetch('size')
-          end
-        end
-        @bids.map do |bid|
-          if bid.fetch(:order_id) == msg.fetch('taker_order_id')
-            bid[:size] = bid[:size] - msg.fetch('size')
-          end
-        end
-      when 'buy'
-        @bids.map do |bid|
-          if bid.fetch(:order_id) == msg.fetch('maker_order_id')
-            bid[:size] = bid[:size] - msg.fetch('size')
-          end
-        end
-        @asks.map do |ask|
-          if ask.fetch(:order_id) == msg.fetch('taker_order_id')
-            ask[:size] = ask[:size] - msg.fetch('size')
-          end
+      decrement_match = lambda do |o|
+        if o.fetch(:order_id) == msg.fetch('maker_order_id')
+          o[:size] = o.fetch(:size) - msg.fetch('size')
         end
       end
+
+      @asks.map &decrement_match if msg.fetch('side') == 'sell'
+      @bids.map &decrement_match if msg.fetch('side') == 'buy'
     end
 
     def done(msg)
-      case msg.fetch('side')
-      when 'sell'
-        @asks.reject! {|a| a.fetch(:order_id) == msg['order_id']}
-      when 'buy'
-        @bids.reject! {|b| b.fetch(:order_id) == msg['order_id']}
-      end
+      matching_order = lambda {|o| o.fetch(:order_id) == msg.fetch('order_id')}
+
+      @asks.reject! &matching_order if msg.fetch('side') == 'sell'
+      @bids.reject! &matching_order if msg.fetch('side') == 'buy'
     end
 
     def change(msg)
+      change_order = lambda do |o|
+        if o.fetch(:order_id) == msg.fetch('order_id')
+          o[:size] = msg.fetch('new_size')
+        end
+      end
+
       case msg.fetch('side')
       when 'sell'
-        @asks.map do |a|
-          if a.fetch(:order_id) == msg.fetch('order_id')
-            a[:size] = msg.fetch('new_size')
-          end
-        end
+        @asks.map &change_order
       when 'buy'
-        @bids.map do |b|
-          if b.fetch(:order_id) == msg.fetch('order_id')
-            b[:size] = msg.fetch('new_size')
-          end
-        end
+        @bids.map &change_order
       end
     end
 
     def received(msg)
       # The book doesn't change for this message type.
     end
-
   end
 end
